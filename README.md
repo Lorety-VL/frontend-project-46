8b8adf277c4658998377ae17ce62bc92a2fa3c62
Lorety-VL <vladimirking18@gmail.com>
2023-03-13 19:44:07 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Lorety-VL/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/Lorety-VL/frontend-project-46/actions)
+
+## Example how it works with flat JSON files:
+
+[![asciicast](https://asciinema.org/a/u30RawLtc65gzng7X920pKHUn.svg)](https://asciinema.org/a/u30RawLtc65gzng7X920pKHUn)
--- bin/gendiff-cli.js
+++ bin/gendiff-cli.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
+import genDiff from '../src/gendiff.js';
 import { Command } from 'commander';
 const program = new Command();
+
 
 program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>');
+    .arguments('<filepath1> <filepath2>')
+    .action((filePath1, filePath2, options) => {
+        console.log(genDiff(filePath1, filePath2));
+    });
 
 program.parse();
