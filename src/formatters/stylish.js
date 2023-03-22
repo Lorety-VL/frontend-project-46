acff7d48b202fcaca9b9be266711cb978a06bb4d
Lorety-VL <vladimirking18@gmail.com>
2023-03-22 19:38:36 +0300

    step 7 fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -20,23 +20,23 @@ const stylish = (tree, depthMain = 1) => {
     const indient = ' '.repeat(4 * depth - 2);
     switch (node.type) {
       case 'nested':
-        return `${indient}  ${node.key}: ${stylish(node.value, depth + 1)}`.trimEnd();
+        return `${indient}  ${node.key}: ${stylish(node.value, depth + 1)}`;
       case 'added':
-        return `${indient}+ ${node.key}: ${toStr(node.value, depth + 1)}`.trimEnd();
+        return `${indient}+ ${node.key}: ${toStr(node.value, depth + 1)}`;
       case 'deleted':
-        return `${indient}- ${node.key}: ${toStr(node.value, depth + 1)}`.trimEnd();
+        return `${indient}- ${node.key}: ${toStr(node.value, depth + 1)}`;
       case 'same':
-        return `${indient}  ${node.key}: ${toStr(node.value, depth + 1)}`.trimEnd();
+        return `${indient}  ${node.key}: ${toStr(node.value, depth + 1)}`;
       case 'diff':
         return [
           `${indient}- ${node.key}: ${toStr(node.value1, depth + 1)}`.trimEnd(),
-          `${indient}+ ${node.key}: ${toStr(node.value2, depth + 1)}`.trimEnd(),
+          `${indient}+ ${node.key}: ${toStr(node.value2, depth + 1)}`,
         ].join('\n');
       default:
         throw new Error('unknown type of key');
     }
   };
-  const str = tree.map((node) => iter(node)).join('\n');
+  const str = tree.map((node) => iter(node).trimEnd()).join('\n');
   return `{\n${str}\n${' '.repeat(4 * (depthMain - 1))}}`;
 };
 
